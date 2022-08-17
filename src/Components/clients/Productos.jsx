8f177a3c4b682d584cde98ab6e8babd9cf012d7b
enzo1991 <enzo.aravena.c@gmail.com>
2022-08-16 23:04:03 -0400

    change text click-buttom

--- src/Components/clients/Productos.jsx
+++ src/Components/clients/Productos.jsx
@@ -1,16 +1,24 @@
+
 import React, { useEffect } from 'react'
 import useAuth from '../../Hooks/useAuth'
 import fotoPrueba from './img/FrenteAVaras.png'
+import {useState} from 'react'
 const Productos = ({productos}) => {
     const {carrito, setCarrito} = useAuth();
-    
+    const [agregando, setAgregando] = useState({cargando: false, id: ''})    
 
     useEffect(() => {
         localStorage.setItem('carrito',JSON.stringify(carrito))
     
     }, [carrito])
     const handleClick = (producto) =>{
+        
+        
+            setAgregando({cargando:true,
+                          id: producto.ID})
+        
         const existe = carrito.some(p => p.ID === producto.ID);
+
 
         if(existe){
             const nuevos = carrito.map(p =>{
@@ -26,10 +34,14 @@ const Productos = ({productos}) => {
         }else{
             setCarrito([...carrito, producto])
         }
-
+        setTimeout(() => {
+            setAgregando({cargando:false,
+                        id: ''})
+        }, 1000);
         // setCarrito([...carrito, producto])
        
     }
+
 
   return (
     <div className='mt-12 grid md:grid-cols-6 sm:gap-7 justify-center  gap-2 grid-cols-2 sm:px-0 px-4'>
@@ -44,8 +56,10 @@ const Productos = ({productos}) => {
                     <p>precio: {PRECIO}</p>
                     </div>
                     <div className='flex justify-center mt-4 '>
-                    <button onClick={(e) => handleClick({ID,NOMBRE, CANTIDAD, PRECIO, unidad: 1},e)} className='bg-blue-600 px-2 py-2 rounded-md'>Agregar al carrito</button>
-
+                    <button onClick={(e) => handleClick({ID,NOMBRE, CANTIDAD, PRECIO, unidad: 1},e)} className='hover:bg-blue-800 bg-blue-600 px-2 py-2 rounded-md'>
+                        {agregando.cargando && agregando.id === ID    ? "Agregando...": "Agregar al carrito"}
+                    </button>
+                    
                     </div>
 
                 </div>
